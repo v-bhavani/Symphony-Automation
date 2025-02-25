ae052274be4710df3562df6b0b7a1cf631fc6eaa
v-bhavani <119451594+v-bhavani@users.noreply.github.com>
2025-02-25 21:58:12 +0530

    Update anugal_ecr_scan.sh

--- anugal_ecr_scan.sh
+++ anugal_ecr_scan.sh
@@ -57,14 +57,14 @@ echo "Inspector report exporting to s3."
 sleep 100
 
 # Run AWS Inspector2 report generation
-report_id=$(aws inspector2 create-findings-report \
+anugal_report_id=$(aws inspector2 create-findings-report \
     --region "$AWS_REGION" \
     --report-format CSV \
     --s3-destination bucketName="$S3_BUCKET_NAME",keyPrefix="$S3_KEY_PREFIX",kmsKeyArn="$KMS_KEY_ARN" \
     --filter-criteria '{ "ecrImageRepositoryName": [{"comparison": "EQUALS", "value": "'"$ECR_REPO_NAME"'"}] }' | awk 'NR==2{ print; exit }' | awk '{print$2}' | tr -d '"')
 
-echo "Report ID is : ${report_id}"
-if [ -z "$report_id" ]; then
+echo "Report ID is : ${anugal_report_id}"
+if [ -z "$anugal_report_id" ]; then
     echo "Failed: Report generation encountered an error."
     exit 1
 fi
